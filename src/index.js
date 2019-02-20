792b2cfbda357ca8c69b0566e1dc28558fc2bb02
abzhapparovmaxat <abzhapparovmaxat@gmail.com>
2019-02-20 22:48:05 +0600

    fix: function getLoveTrianglesCount in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let trinagles = spichonee1 = spichonee2 = spichonee3 = 0;
+  for (i = 0; i < preferences.length; i++) {
+    spichonee1 = preferences[i];
+    spichonee2 = preferences[spichonee1-1];
+    spichonee3 = preferences[spichonee2-1];
+    if(spichonee3 == i+1) trinagles++;
+  }
+  trinagles = Math.floor(trinagles/3);
+  return trinagles;
 };
